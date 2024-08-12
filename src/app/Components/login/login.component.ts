ca9cbcd2a0484b3f763d15f9fef4e76d8ed71142
smash3r <bhubanmagar79@gmail.com>
2024-08-12 17:54:48 +0545

    login form

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(5),
+  ]);
+  loginForm = new FormGroup({
+    email: this.email,
+    password: this.password,
+  });
+
+  login() {
+    console.log(this.loginForm.value);
+  }
+}
